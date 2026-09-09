5d5c23c1c9f81d06a7fa7ecf34837101b620381e
Nilay Shrivastava <nilay.euler16@gmail.com>
2026-09-09 23:52:53 +0100

    error handling

--- fp_in_scala/error_handling.scala
+++ fp_in_scala/error_handling.scala
@@ -39,13 +39,58 @@ object Chapter4:
 
   def lift[A, B](f: A=>B): Option[A]=>Option[B] = 
     _.map(f) // this is the shorthand for a: Option[A] => a.map(f)
-    
+   
+  def map2[A, B, C](a : Option[A], b: Option[B])(f: (A, B) => C): Option[C] = 
+    /*
+    (a, b) match 
+      case (None, _) => None
+      case (_, None) => None
+      case _ => Some(f(a, b))
+    */
+    // better below
+    /*(a, b) match
+      case (Some(aa), Some(bb)) => Some(f(aa, bb))
+      case _ => None
+    */
+    // even better
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
+
+  def sequence[A](as: List[Option[A]]):Option[List[A]] =
+    as match
+      case Nil => Option.Some(Nil)
+      case head::tail => head match
+                            case Option.None => Option.None
+                            case Option.Some(hh) => sequence(tail) match
+                                                case Option.None => Option.None
+                                                case Option.Some(tt) => Option.Some(hh::tt)
+
+
+  def sequence2[A](as: List[Option[A]]): Option[List[A]] = 
+    as match
+      case Nil => Option.Some(Nil)
+      case head::tail => head.flatMap(hh => sequence2(tail).map(tt => (hh::tt)))
+
+  /*
+  def sequence3[A](as: List[Option[A]]): Option[List[A]] = 
+    foldRight(as, Some(Nil), (head, accumulated) => head.flatMap(hh => accumulated.map(tt => (hh :: tt))))
+  */
+  def traverseExistingOptions[A, B](as: List[Option[A]])(f: A => Option[B]): Option[List[B]] = 
+    as.foldRight(Option.Some(Nil))((head, accumulated) => {
+      head.flatMap(f).flatMap(hh => accumulated.map(tt => (hh::tt)))
+    })
+
+  def 
+
+
+
 @main def printer(): Unit = 
   import Chapter4.*
 
 
   val l = List(1.0, 2.0, 3.0, 4.0)
   println(variance(l).getOrElse(0))
-     
+  val p : List[Option[Int]] = List(Option.Some(1), Option.Some(2), Option.Some(3))   
+  // println(sequence3(p))
+  println(traverseExisting(p)(a => Option.Some(a)))
 
 
